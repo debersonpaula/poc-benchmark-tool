dcaeff992ffa2de270bf1980f515c4ff642b164c
Deberson de Paula <deberson.paula@gmail.com>
2021-10-03 11:53:50 -0300

    updated sort bench

--- samples/sort-benchmark.js
+++ samples/sort-benchmark.js
@@ -9,13 +9,6 @@ const benchmarktool = require("../tool");
 
 const list = [10, 50, 30, 70, 80, 60, 20, 90, 40, 1, 9, 7, 6, 2, 1, 7, 900, 0];
 
-// const loops = 1000000;
-
-// testcase("insertionSort", () => insertionSort(list), loops);
-// testcase("selectionSort", () => selectionSort(list), loops);
-// testcase("bubbleSort", () => bubbleSort(list), loops);
-// testcase("quickSort", () => quickSort(list, 0, list.length - 1), loops);
-
 benchmarktool(() => insertionSort(list), {
   name: insertionSort.name,
   steps: 10,
@@ -24,6 +17,18 @@ benchmarktool(() => insertionSort(list), {
 
 benchmarktool(() => selectionSort(list), {
   name: selectionSort.name,
+  steps: 10,
+  loops: 1000000,
+});
+
+benchmarktool(() => bubbleSort(list), {
+  name: bubbleSort.name,
+  steps: 10,
+  loops: 1000000,
+});
+
+benchmarktool(() => quickSort(list, 0, list.length - 1), {
+  name: quickSort.name,
   steps: 10,
   loops: 1000000,
 });
